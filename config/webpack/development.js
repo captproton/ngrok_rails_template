80e4e64332e89956632a9b17e04ea00b9cbe12e6
captproton <carl@wdwhub.net>
2020-10-24 10:17:02 -0700

    FIXED: Webpack Dev Server was disconnecting. Port was changed for SSL

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -9,10 +9,14 @@ dotenv.config({ path: ".env", silent: true });
 
 const ngrokTunnel = process.env.ASSET_TUNNEL_URI;
 
+// Webpack Dev Server (WDS) is usually 3035.
+// Because we are accessing WDS through ngrok, everything is https.
+// HTTPS is normally port 443.
+// This also means that this config w/o more work is ngrok all the time.
 environment.config.merge({
   devServer: {
-    host: ngrokTunnel,
-    public: ngrokTunnel + ":3035",
+    port: "443",
+    public: ngrokTunnel,
   },
 });
 
